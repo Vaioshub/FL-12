b4b97a5280e71a87e3cde944f2fc2384c551cf32
Vaios <vaioshub@gmail.com>
2019-12-24 23:24:12 +0200

    HW6 finished two parts

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,7 +1,22 @@
-let a=0, b=0, c=0;
-a = +prompt("Enter first value");
-b = +prompt("Enter second value");
-c = +prompt("Enter third value");
-console.log(typeof(a));
-// if(typ a || b)
+let a, b, c;
+a = +prompt("Enter first value for the quadratic equation a");
+b = +prompt("Enter second value for the quadratic equation b");
+c = +prompt("Enter third value for the quadratic equation c");
 
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    console.log("Invalid input data");
+} else {
+    let discriminant = b * b - 4 * a * c;
+    console.log("disc:" + discriminant);
+    if (discriminant < 0) {
+        console.log("no solution");
+    } else if (discriminant === 0) {
+        let x = - b / (2 * a);
+        console.log("X = " + x);
+    } else {
+        let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        console.log("X1 = " + x1);
+        console.log("X2 = " + x2);
+    }
+}
